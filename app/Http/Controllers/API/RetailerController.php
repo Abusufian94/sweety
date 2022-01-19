37af4d102148722403e7268486d7d12822b8e7de
Prakash <prakash@unifiedinfotech.net>
2022-01-20 00:52:28 +0530

    add retails product list

--- app/Http/Controllers/API/RetailerController.php
+++ app/Http/Controllers/API/RetailerController.php
@@ -11,14 +11,14 @@ class RetailerController extends Controller
     //
     public function retailuserproducts($user_id) {
         try  {
-          $user = \DB::table('users')->select('roles')->where('id','=',$user_id)->first();
-          if($user->roles == 1){
-              $products = \DB::table('product')->select('*')->get();
+          $user = \DB::table('users')->select('role')->where('id','=',$user_id)->first();
+          if($user->role == 1){
+              $products = \DB::table('products')->select('*')->get();
               return response()->json(['status'=>true,'message' =>'Products has been fetched successfully','data'=>$products,'error'=>[]]);
           }
-          else if($user->roles == 2) {
-               $products  = \DB::table('product')->select('product.*','product_retail_assign_log.*')->leftJoin('product_retail_assign_log',function($join) {
-                  $join->on('product.id','=','product_retail_assign_log.product_id');
+          else if($user->role == 2) {
+               $products  = \DB::table('products')->select('products.*','product_retail_assign_log.*')->leftJoin('product_retail_assign_log',function($join) {
+                  $join->on('products.id','=','product_retail_assign_log.product_id');
                })->where('product_retail_assign_log.user_id','=',$user_id)->get();
                return response()->json(['status'=>true,'message' =>'Products has been fetched successfully','data'=>$products,'error'=>(object)[]]);
           }
