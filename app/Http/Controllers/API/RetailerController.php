1d4cf75b9f21782f1cceeccc16b22e4007f2a1fd
Prakash <prakash@unifiedinfotech.net>
2022-01-20 01:07:49 +0530

    retailer product list query modified

--- app/Http/Controllers/API/RetailerController.php
+++ app/Http/Controllers/API/RetailerController.php
@@ -4,24 +4,24 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\User;
+
+use App\Product;
 use DB;
 class RetailerController extends Controller
 {
     //
     public function retailuserproducts($user_id) {
         try  {
-          $user = \DB::table('users')->select('roles')->where('id','=',$user_id)->first();
-          if($user->roles == 1){
-              $products = \DB::table('product')->select('*')->get();
-              return response()->json(['status'=>true,'message' =>'Products has been fetched successfully','data'=>$products,'error'=>[]]);
+          $user = \DB::table('users')->select('roles as role')->where('id','=',$user_id)->first();
+          $products = Product::select('*');
+          if($user->role == 2){
+            $products = $products->leftJoin('product_retail_assign_log',function($join) {
+             $join->on('products.id','=','product_retail_assign_log.product_id');
+            })->where('product_retail_assign_log.user_id','=',$user_id);
           }
-          else if($user->roles == 2) {
-               $products  = \DB::table('product')->select('product.*','product_retail_assign_log.*')->leftJoin('product_retail_assign_log',function($join) {
-                  $join->on('product.id','=','product_retail_assign_log.product_id');
-               })->where('product_retail_assign_log.user_id','=',$user_id)->get();
-               return response()->json(['status'=>true,'message' =>'Products has been fetched successfully','data'=>$products,'error'=>(object)[]]);
-          }
+          $products = $products->get();
+          return response()->json(['status'=>true,'message' =>'Products has been fetched successfully','data'=>$products,'error'=>(object)[]]);
+
         } catch (\Exception $ex) {
             return response()->json(['status'=>true,'message'=>$ex->getMessage(),'date'=>(object)[]]);
         }
