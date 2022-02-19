9024f45463c85ac86340a1299a834dc34519e4c9
Abu Sufin Kayal <kayal.sufian.abu@gmail.com>
2022-02-20 01:13:31 +0530

    route updated

--- routes/api.php
+++ routes/api.php
@@ -63,17 +63,10 @@ Route::group(["prefix" => "/v1", "middleware" => ['auth:api'], "namespace" => '\
        Route::delete('admin/retail_user/delete', 'UserController@retailUserDelete');
        Route::get('retailer/assign/products/','RetailerController@retailuserproducts')->name('retail.assign.products');
 
-
-
-
-  /*retail store list*/
-  Route::get('admin/retails', 'StockController@retailList');
-  Route::post('admin/retail-user/create', 'UserController@retailUserCreate');
-  Route::get('admin/retail-users', 'UserController@retailUsers');
   Route::get('/retail-users', 'Warehouseroducts@retailUserList')->name('retailusers');
   Route::get('/retail-products', 'RetailProducts@RetailProducts')->name('retail.products');
   Route::post('/retail-products-approve', 'RetailProducts@approveProduct')->name('retail.approve');
-   route::get('retailer/assign/products/','retailercontroller@retailuserproducts')->name('retail.assign.products');
+  
 
      Route::get('/product-retail-list/{status?}', 'Warehouseroducts@productRetailList')->name('product.retail.list');
 });
